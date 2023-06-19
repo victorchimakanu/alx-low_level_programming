d0f05180d08dd4a2f33e2aea4a210c3bafa94dec
Victor Kanu <victorchimakanu@gmail.com>
2023-06-19 05:37:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * return: 1
  */
 
@@ -10,4 +10,5 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+
+}

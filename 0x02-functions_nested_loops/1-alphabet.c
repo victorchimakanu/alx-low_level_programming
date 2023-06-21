a1d36bac7776c63a7ae524dbafd296634c14051b
Victor Kanu <victorchimakanu@gmail.com>
2023-06-21 12:43:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet, in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		_putchar(alphabets);
+
+	_putchar('\n');
+}

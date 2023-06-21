5828e9961821b395ab736be4e8e1de475b6ce2e9
Victor Kanu <victorchimakanu@gmail.com>
2023-06-21 12:39:43 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - program prints _putchar
+ *return: 0
+ */
+
+int main(void)
+{
+	
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}

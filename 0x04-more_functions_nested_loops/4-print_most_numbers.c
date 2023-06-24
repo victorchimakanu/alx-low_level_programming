87a4b042213103804557ca210eae485884713f0e
Victor Kanu <victorchimakanu@gmail.com>
2023-06-23 17:48:46 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0-9 
+ */
+
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+			_putchar((c % 10) + '0');
+	}
+
+	_putchar('\n');
+}

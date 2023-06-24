2368289428644361bcc84f865d3000fe52152376
Victor Kanu <victorchimakanu@gmail.com>
2023-06-23 17:46:56 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar((c % 10) + '0');
+
+	_putchar('\n');
+}
